77050ba14c751c27303587d1b0a3288d7ed76181
hiworth <hiworth.hiworth@gmail.com>
2023-01-20 18:19:15 +0530

    "event_management dashboard"

--- event_management/controllers/main.py
+++ event_management/controllers/main.py
@@ -96,13 +96,40 @@ class VenueListingPage(http.Controller):
     @http.route(['/venuelist'], type='http', auth="public",website=True)
     def venue_page_controller(self):
 
-        venues = request.env['res.partner'].sudo().search([])
+        trivandrum_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',1)])
+        kollam_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',2)])
+        pathanamthitta_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',3)])
+        alappey_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',4)])
+        kottayam_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',5)])
+        idukki_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',6)])
+        ernakulam_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',7)])
+        thrissur_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',8)])
+        palakkad_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',9)])
+        malappuram_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',10)])
+        kozhikkod_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',11)])
+        wayanad_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',12)])
+        kannur_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',13)])
+        kasaragod_venues = request.env['res.partner'].sudo().search([('venue','=',True), ('district_id.id','=',14)])
+
         districts = request.env['place.district'].sudo().search([])
         # types = request.env['event.management.type'].search([])
         # places = request.env['place.place'].search([])
 
         values = {
-            'venues':venues,
+            'trivandrum_venues':trivandrum_venues,
+            'kollam_venues':kollam_venues,
+            'pathanamthitta_venues':pathanamthitta_venues,
+            'alappey_venues':alappey_venues,
+            'kottayam_venues':kottayam_venues,
+            'idukki_venues':idukki_venues,
+            'ernakulam_venues':ernakulam_venues,
+            'thrissur_venues':thrissur_venues,
+            'palakkad_venues':palakkad_venues,
+            'malappuram_venues':malappuram_venues,
+            'kozhikkod_venues':kozhikkod_venues,
+            'wayanad_venues':wayanad_venues,
+            'kannur_venues':kannur_venues,
+            'kasaragod_venues':kasaragod_venues,
             'districts':districts,
             # 'types':types,
             # 'places':places,
@@ -118,7 +145,7 @@ class SearchListPage(http.Controller):
         domain_venue = []
         print (args.get('district_id'))
         if args.get('district_id'):
-            domain_venue = [('district_id.id','=',args.get('district_id'))]
+            domain_venue = [('venue','=',True),('district_id.id','=',args.get('district_id'))]
             district_domain = [('id','=',args.get('district_id'))]
 
         venues = request.env['res.partner'].sudo().search(domain_venue)
@@ -137,14 +164,15 @@ class MakeupPage(http.Controller):
     def search_page_controller(self, **args):
 
         print (args.get('price'))
-        makeup_artists = request.env['artist.artist'].sudo().search([])
-        mehandi_artists = request.env['artist.artist'].sudo().search([])
+        # makeup_artists = request.env['artist.artist'].sudo().search([('makeup_artist','=',True)])
+        makeup_artists = request.env['res.partner'].sudo().search([('makeup_artist','=',True)])
+        mehndi_artists = request.env['res.partner'].sudo().search([('mehndi_artist','=',True)])
         packages = request.env['makeup.package'].sudo().search([])
         services = request.env['package.service'].sudo().search([])
 
         values = {
             'makeup_artists':makeup_artists,
-            'mehandi_artists':mehandi_artists,
+            'mehndi_artists':mehndi_artists,
             'packages':packages,
             'services':services,
         }
@@ -158,7 +186,7 @@ class EventDetailsPage(http.Controller):
         # print ('..........')
         # print (district_id)
         print (args.get('price'))
-        venues = request.env['res.partner'].sudo().search([('district_id','=',args.get('district_id'))])
+        venues = request.env['res.partner'].sudo().search([('venue','=',True),('district_id','=',args.get('district_id'))])
         # venues = request.env['res.partner'].search([])
         districts = request.env['place.district'].sudo().search([('id','=',args.get('district_id'))])
         # types = request.env['event.management.type'].search([])
@@ -176,7 +204,7 @@ class EventDetailsPage(http.Controller):
 class BookingPage(http.Controller):
     @http.route(['/booking'], type='http', auth="public",website=True)
     def booking_page_controller(self, **args):
-        venues = request.env['res.partner'].sudo().search([])
+        venues = request.env['res.partner'].sudo().search([('venue','=',True)])
         districts = request.env['place.district'].sudo().search([])
         types = request.env['event.management.type'].sudo().search([])
         places = request.env['place.place'].sudo().search([])
@@ -190,3 +218,22 @@ class BookingPage(http.Controller):
         return request.render(
 
         "event_management.booking_page",values)
+
+
+class RegistrationPage(http.Controller):
+    @http.route(['/registration'], type='http', auth="public",website=True)
+    def booking_page_controller(self, **args):
+        # venues = request.env['res.partner'].sudo().search([('venue','=',True),])
+        # districts = request.env['place.district'].sudo().search([])
+        # types = request.env['event.management.type'].sudo().search([])
+        # places = request.env['place.place'].sudo().search([])
+
+        values = {
+            # 'venues':venues,
+            # 'districts':districts,
+            # 'types':types,
+            # 'places':places,
+        }
+        return request.render(
+
+        "event_management.registration_page",values)
