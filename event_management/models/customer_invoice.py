273238d614e3c79a48d3eddd1629c761be389a44
hiworth <hiworth.hiworth@gmail.com>
2023-03-09 19:02:09 +0530

    "event_management 09/03"

--- event_management/models/customer_invoice.py
+++ event_management/models/customer_invoice.py
@@ -366,13 +366,6 @@ class AccountMoveLine(models.Model):
 
     advance = fields.Float()
     fortuna_discount_line = fields.Float()
-    # fortuna_discount_line1 = fields.Float(compute="onchange_fortuna_discount_line")
-    fortuna_discount_line1 = fields.Float()
-
-    @api.depends('fortuna_discount_line')
-    def onchange_fortuna_discount_line(self):
-        for rec in self:
-            rec.fortuna_discount_line1 = rec.fortuna_discount_line
 
     @api.onchange('quantity', 'discount', 'price_unit', 'tax_ids','fortuna_discount_line','advance')
     def _onchange_price_subtotal(self):
