24d3bc035711e68ee4e97c586334ee9bd993a126
hiworth <hiworth.hiworth@gmail.com>
2023-03-09 18:14:50 +0530

    "event_management 09/03"

--- event_management/models/customer_invoice.py
+++ event_management/models/customer_invoice.py
@@ -4,13 +4,15 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
-
+INTEGRITY_HASH_MOVE_FIELDS = ('date', 'journal_id', 'company_id')
+INTEGRITY_HASH_LINE_FIELDS = ('debit', 'credit', 'account_id', 'partner_id')
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     booking_id = fields.Many2one('event.management',required=True)
     auditorium_id = fields.Many2one('res.partner', related='booking_id.venue_id')
     fortuna_discount = fields.Float()
+    auditorium_discount = fields.Float()
     address = fields.Text()
     total_advance = fields.Float()
     user_id = fields.Many2one('res.users', default=lambda self: self.env.user.id)
@@ -149,6 +151,198 @@ class AccountMove(models.Model):
 
             move.payment_state = new_pmt_state
 
+
+    def _recompute_tax_lines(self, recompute_tax_base_amount=False, tax_rep_lines_to_recompute=None):
+        """ Compute the dynamic tax lines of the journal entry.
+
+        :param recompute_tax_base_amount: Flag forcing only the recomputation of the `tax_base_amount` field.
+        """
+        self.ensure_one()
+        in_draft_mode = self != self._origin
+
+        def _serialize_tax_grouping_key(grouping_dict):
+            ''' Serialize the dictionary values to be used in the taxes_map.
+            :param grouping_dict: The values returned by '_get_tax_grouping_key_from_tax_line' or '_get_tax_grouping_key_from_base_line'.
+            :return: A string representing the values.
+            '''
+            return '-'.join(str(v) for v in grouping_dict.values())
+
+        def _compute_base_line_taxes(base_line):
+            ''' Compute taxes amounts both in company currency / foreign currency as the ratio between
+            amount_currency & balance could not be the same as the expected currency rate.
+            The 'amount_currency' value will be set on compute_all(...)['taxes'] in multi-currency.
+            :param base_line:   The account.move.line owning the taxes.
+            :return:            The result of the compute_all method.
+            '''
+            move = base_line.move_id
+
+            if move.is_invoice(include_receipts=True):
+                handle_price_include = True
+                sign = -1 if move.is_inbound() else 1
+                quantity = base_line.quantity
+                is_refund = move.move_type in ('out_refund', 'in_refund')
+                price_unit_wo_discount = sign * (base_line.price_unit * (1 - (base_line.discount / 100.0)))* (1 - (base_line.fortuna_discount_line / 100.0))
+            else:
+                handle_price_include = False
+                quantity = 1.0
+                tax_type = base_line.tax_ids[0].type_tax_use if base_line.tax_ids else None
+                is_refund = (tax_type == 'sale' and base_line.debit) or (tax_type == 'purchase' and base_line.credit)
+                price_unit_wo_discount = base_line.amount_currency
+
+            balance_taxes_res = base_line.tax_ids._origin.with_context(force_sign=move._get_tax_force_sign()).compute_all(
+                price_unit_wo_discount,
+                currency=base_line.currency_id,
+                quantity=quantity,
+                product=base_line.product_id,
+                partner=base_line.partner_id,
+                is_refund=is_refund,
+                handle_price_include=handle_price_include,
+            )
+
+            if move.move_type == 'entry':
+                repartition_field = is_refund and 'refund_repartition_line_ids' or 'invoice_repartition_line_ids'
+                repartition_tags = base_line.tax_ids.flatten_taxes_hierarchy().mapped(repartition_field).filtered(lambda x: x.repartition_type == 'base').tag_ids
+                tags_need_inversion = self._tax_tags_need_inversion(move, is_refund, tax_type)
+                if tags_need_inversion:
+                    balance_taxes_res['base_tags'] = base_line._revert_signed_tags(repartition_tags).ids
+                    for tax_res in balance_taxes_res['taxes']:
+                        tax_res['tag_ids'] = base_line._revert_signed_tags(self.env['account.account.tag'].browse(tax_res['tag_ids'])).ids
+
+            return balance_taxes_res
+
+        taxes_map = {}
+
+        # ==== Add tax lines ====
+        to_remove = self.env['account.move.line']
+        for line in self.line_ids.filtered('tax_repartition_line_id'):
+            grouping_dict = self._get_tax_grouping_key_from_tax_line(line)
+            grouping_key = _serialize_tax_grouping_key(grouping_dict)
+            if grouping_key in taxes_map:
+                # A line with the same key does already exist, we only need one
+                # to modify it; we have to drop this one.
+                to_remove += line
+            else:
+                taxes_map[grouping_key] = {
+                    'tax_line': line,
+                    'amount': 0.0,
+                    'tax_base_amount': 0.0,
+                    'grouping_dict': False,
+                }
+        if not recompute_tax_base_amount:
+            self.line_ids -= to_remove
+
+        # ==== Mount base lines ====
+        for line in self.line_ids.filtered(lambda line: not line.tax_repartition_line_id):
+            # Don't call compute_all if there is no tax.
+            if not line.tax_ids:
+                if not recompute_tax_base_amount:
+                    line.tax_tag_ids = [(5, 0, 0)]
+                continue
+
+            compute_all_vals = _compute_base_line_taxes(line)
+
+            # Assign tags on base line
+            if not recompute_tax_base_amount:
+                line.tax_tag_ids = compute_all_vals['base_tags'] or [(5, 0, 0)]
+
+            tax_exigible = True
+            for tax_vals in compute_all_vals['taxes']:
+                grouping_dict = self._get_tax_grouping_key_from_base_line(line, tax_vals)
+                grouping_key = _serialize_tax_grouping_key(grouping_dict)
+
+                tax_repartition_line = self.env['account.tax.repartition.line'].browse(tax_vals['tax_repartition_line_id'])
+                tax = tax_repartition_line.invoice_tax_id or tax_repartition_line.refund_tax_id
+
+                if tax.tax_exigibility == 'on_payment':
+                    tax_exigible = False
+
+                taxes_map_entry = taxes_map.setdefault(grouping_key, {
+                    'tax_line': None,
+                    'amount': 0.0,
+                    'tax_base_amount': 0.0,
+                    'grouping_dict': False,
+                })
+                taxes_map_entry['amount'] += tax_vals['amount']
+                taxes_map_entry['tax_base_amount'] += self._get_base_amount_to_display(tax_vals['base'], tax_repartition_line, tax_vals['group'])
+                taxes_map_entry['grouping_dict'] = grouping_dict
+            if not recompute_tax_base_amount:
+                line.tax_exigible = tax_exigible
+
+        # ==== Pre-process taxes_map ====
+        taxes_map = self._preprocess_taxes_map(taxes_map)
+
+        # ==== Process taxes_map ====
+        for taxes_map_entry in taxes_map.values():
+            # The tax line is no longer used in any base lines, drop it.
+            if taxes_map_entry['tax_line'] and not taxes_map_entry['grouping_dict']:
+                if not recompute_tax_base_amount:
+                    self.line_ids -= taxes_map_entry['tax_line']
+                continue
+
+            currency = self.env['res.currency'].browse(taxes_map_entry['grouping_dict']['currency_id'])
+
+            # Don't create tax lines with zero balance.
+            if currency.is_zero(taxes_map_entry['amount']):
+                if taxes_map_entry['tax_line'] and not recompute_tax_base_amount:
+                    self.line_ids -= taxes_map_entry['tax_line']
+                continue
+
+            # tax_base_amount field is expressed using the company currency.
+            tax_base_amount = currency._convert(taxes_map_entry['tax_base_amount'], self.company_currency_id, self.company_id, self.date or fields.Date.context_today(self))
+
+            # Recompute only the tax_base_amount.
+            if recompute_tax_base_amount:
+                if taxes_map_entry['tax_line']:
+                    taxes_map_entry['tax_line'].tax_base_amount = tax_base_amount
+                continue
+
+            balance = currency._convert(
+                taxes_map_entry['amount'],
+                self.company_currency_id,
+                self.company_id,
+                self.date or fields.Date.context_today(self),
+            )
+            to_write_on_line = {
+                'amount_currency': taxes_map_entry['amount'],
+                'currency_id': taxes_map_entry['grouping_dict']['currency_id'],
+                'debit': balance > 0.0 and balance or 0.0,
+                'credit': balance < 0.0 and -balance or 0.0,
+                'tax_base_amount': tax_base_amount,
+            }
+
+            if taxes_map_entry['tax_line']:
+                # Update an existing tax line.
+                if tax_rep_lines_to_recompute and taxes_map_entry['tax_line'].tax_repartition_line_id not in tax_rep_lines_to_recompute:
+                    continue
+
+                taxes_map_entry['tax_line'].update(to_write_on_line)
+            else:
+                # Create a new tax line.
+                create_method = in_draft_mode and self.env['account.move.line'].new or self.env['account.move.line'].create
+                tax_repartition_line_id = taxes_map_entry['grouping_dict']['tax_repartition_line_id']
+                tax_repartition_line = self.env['account.tax.repartition.line'].browse(tax_repartition_line_id)
+
+                if tax_rep_lines_to_recompute and tax_repartition_line not in tax_rep_lines_to_recompute:
+                    continue
+
+                tax = tax_repartition_line.invoice_tax_id or tax_repartition_line.refund_tax_id
+                taxes_map_entry['tax_line'] = create_method({
+                    **to_write_on_line,
+                    'name': tax.name,
+                    'move_id': self.id,
+                    'company_id': self.company_id.id,
+                    'company_currency_id': self.company_currency_id.id,
+                    'tax_base_amount': tax_base_amount,
+                    'exclude_from_invoice_tab': True,
+                    'tax_exigible': tax.tax_exigibility == 'on_invoice',
+                    **taxes_map_entry['grouping_dict'],
+                })
+
+            if in_draft_mode:
+                taxes_map_entry['tax_line'].update(taxes_map_entry['tax_line']._get_fields_onchange_balance(force_computation=True))
+
+
+
     @api.model
     def create(self, vals_list):
         res = super(AccountMove,self).create(vals_list)
@@ -171,9 +365,9 @@ class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     advance = fields.Float()
-    fortuna_discount = fields.Float(force_save=True)
+    fortuna_discount_line = fields.Float()
 
-    @api.onchange('quantity', 'discount', 'price_unit', 'tax_ids','fortuna_discount','advance')
+    @api.onchange('quantity', 'discount', 'price_unit', 'tax_ids','fortuna_discount_line','advance')
     def _onchange_price_subtotal(self):
         for line in self:
             if not line.move_id.is_invoice(include_receipts=True):
@@ -184,7 +378,7 @@ class AccountMoveLine(models.Model):
 
 
 
-    def _get_price_total_and_subtotal(self, price_unit=None, quantity=None, discount=None, advance=None, fortuna_discount=None, currency=None, product=None, partner=None, taxes=None, move_type=None):
+    def _get_price_total_and_subtotal(self, price_unit=None, quantity=None, discount=None, advance=None, fortuna_discount_line=None, currency=None, product=None, partner=None, taxes=None, move_type=None):
         self.ensure_one()
         return self._get_price_total_and_subtotal_model(
             price_unit=self.price_unit if price_unit is None else price_unit,
@@ -196,14 +390,14 @@ class AccountMoveLine(models.Model):
             taxes=self.tax_ids if taxes is None else taxes,
             move_type=self.move_id.move_type if move_type is None else move_type,
             advance=self.advance if advance is None else advance,
-            fortuna_discount=self.fortuna_discount if fortuna_discount is None else fortuna_discount,
+            fortuna_discount_line=self.fortuna_discount_line if fortuna_discount_line is None else fortuna_discount_line,
 
         )
 
 
 
     @api.model
-    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, advance, fortuna_discount, currency, product, partner, taxes, move_type):
+    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, advance, fortuna_discount_line, currency, product, partner, taxes, move_type):
         ''' This method is used to compute 'price_total' & 'price_subtotal'.
 
         :param price_unit:  The current price unit.
@@ -219,7 +413,7 @@ class AccountMoveLine(models.Model):
         res = {}
 
         # Compute 'price_subtotal'.
-        line_discount_price_unit = (price_unit * (1 - (discount / 100.0)))*(1 - (fortuna_discount / 100.0))
+        line_discount_price_unit = (price_unit * (1 - (discount / 100.0)))*(1 - (fortuna_discount_line / 100.0))
 
         subtotal = quantity * line_discount_price_unit
         # subtotal = (quantity * line_discount_price_unit) - advance
@@ -236,12 +430,12 @@ class AccountMoveLine(models.Model):
         if currency:
             res = {k: currency.round(v) for k, v in res.items()}
         return res
-    def _get_fields_onchange_balance(self, quantity=None, discount=None, advance=None, fortuna_discount=None, amount_currency=None, move_type=None, currency=None, taxes=None, price_subtotal=None, force_computation=False):
+    def _get_fields_onchange_balance(self, quantity=None, discount=None, advance=None, fortuna_discount_line=None, amount_currency=None, move_type=None, currency=None, taxes=None, price_subtotal=None, force_computation=False):
         self.ensure_one()
         return self._get_fields_onchange_balance_model(
             quantity=self.quantity if quantity is None else quantity,
             advance=self.advance if advance is None else advance,
-            fortuna_discount=self.fortuna_discount if fortuna_discount is None else fortuna_discount,
+            fortuna_discount_line=self.fortuna_discount_line if fortuna_discount_line is None else fortuna_discount_line,
             discount=self.discount if discount is None else discount,
             amount_currency=self.amount_currency if amount_currency is None else amount_currency,
             move_type=self.move_id.move_type if move_type is None else move_type,
@@ -253,7 +447,7 @@ class AccountMoveLine(models.Model):
 
 
     @api.model
-    def _get_fields_onchange_balance_model(self, quantity, discount, advance, fortuna_discount, amount_currency, move_type, currency, taxes, price_subtotal, force_computation=False):
+    def _get_fields_onchange_balance_model(self, quantity, discount, advance, fortuna_discount_line, amount_currency, move_type, currency, taxes, price_subtotal, force_computation=False):
         ''' This method is used to recompute the values of 'quantity', 'discount', 'price_unit' due to a change made
         in some accounting fields such as 'balance'.
 
@@ -314,7 +508,7 @@ class AccountMoveLine(models.Model):
             # discount != 100%
             vals = {
                 'quantity': quantity or 1.0,
-                'fortuna_discount':fortuna_discount,
+                'fortuna_discount_line':fortuna_discount_line,
                 # 'price_unit': amount_currency / discount_factor / (quantity or 1.0),
             }
         elif amount_currency and not discount_factor:
@@ -322,7 +516,7 @@ class AccountMoveLine(models.Model):
             vals = {
                 'quantity': quantity or 1.0,
                 'discount': 0.0,
-                'fortuna_discount':fortuna_discount,
+                'fortuna_discount_line':fortuna_discount_line,
                 # 'price_unit': amount_currency / (quantity or 1.0),
             }
         elif not discount_factor:
@@ -337,7 +531,7 @@ class AccountMoveLine(models.Model):
     def create(self, vals_list):
         # OVERRIDE
         ACCOUNTING_FIELDS = ('debit', 'credit', 'amount_currency')
-        BUSINESS_FIELDS = ('price_unit', 'quantity', 'discount', 'tax_ids','fortuna_discount')
+        BUSINESS_FIELDS = ('price_unit', 'quantity', 'discount', 'tax_ids','fortuna_discount_line')
 
         for vals in vals_list:
             move = self.env['account.move'].browse(vals['move_id'])
@@ -372,7 +566,7 @@ class AccountMoveLine(models.Model):
                         vals.get('quantity', 0.0),
                         vals.get('discount', 0.0),
                         vals.get('advance', 0.0),
-                        vals.get('fortuna_discount', 0.0),
+                        vals.get('fortuna_discount_line', 0.0),
                         currency,
                         self.env['product.product'].browse(vals.get('product_id')),
                         partner,
@@ -383,7 +577,7 @@ class AccountMoveLine(models.Model):
                         vals.get('quantity', 0.0),
                         vals.get('discount', 0.0),
                         vals.get('advance', 0.0),
-                        vals.get('fortuna_discount', 0.0),
+                        vals.get('fortuna_discount_line', 0.0),
                         vals['amount_currency'],
                         move.move_type,
                         currency,
@@ -395,7 +589,7 @@ class AccountMoveLine(models.Model):
                         vals.get('quantity', 0.0),
                         vals.get('discount', 0.0),
                         vals.get('advance', 0.0),
-                        vals.get('fortuna_discount', 0.0),
+                        vals.get('fortuna_discount_line', 0.0),
                         currency,
                         self.env['product.product'].browse(vals.get('product_id')),
                         partner,
@@ -408,7 +602,7 @@ class AccountMoveLine(models.Model):
                         vals.get('quantity', 0.0),
                         vals.get('discount', 0.0),
                         vals.get('advance', 0.0),
-                        vals.get('fortuna_discount', 0.0),
+                        vals.get('fortuna_discount_line', 0.0),
                         currency,
                         self.env['product.product'].browse(vals.get('product_id')),
                         partner,
@@ -522,7 +716,7 @@ class AccountMoveLine(models.Model):
                     quantity=to_write.get('quantity', line.quantity),
                     discount=to_write.get('discount', line.discount),
                     advance=to_write.get('advance', line.advance),
-                    fortuna_discount=to_write.get('fortuna_discount', line.fortuna_discount),
+                    fortuna_discount_line=to_write.get('fortuna_discount_line', line.fortuna_discount_line),
                 ))
                 print("Inside write function.....................")
                 result |= super(AccountMoveLine, line).write(to_write)
