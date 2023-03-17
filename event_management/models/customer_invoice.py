cc5cb38326c00f45dfd545777066fa9a31d0e752
hiworth <hiworth.hiworth@gmail.com>
2023-03-17 15:35:38 +0530

    "event_management 16/03"

--- event_management/models/customer_invoice.py
+++ event_management/models/customer_invoice.py
@@ -457,7 +457,7 @@ class AccountMoveLine(models.Model):
         lines = super(AccountMoveLine, self).create(vals_list)
         for line in lines:
                 line.update(line._get_price_total_and_subtotal())
-                line.update(line._get_fields_onchange_subtotal())
+                # line.update(line._get_fields_onchange_subtotal())
         return lines
 
 #################################################################################################
